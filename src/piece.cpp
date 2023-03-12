0cfb39450146dd063ac23996aac49e8e505a8474
ArcticLampyrid <ArcticLampyrid@outlook.com>
2023-03-12 15:39:21 +0800

    fix: avoid L"" for platform compatibility

--- src/piece.cpp
+++ src/piece.cpp
@@ -4,20 +4,20 @@
 using Type = Piece::PieceType;
 
 const std::map<Type, char16_t> Piece::dict = {
-    { Type::RED_SHUAI, L'帥' },
-    { Type::BLACK_JIANG, L'將' },
-    { Type::RED_SHI, L'仕' },
-    { Type::BLACK_SHI, L'士' },
-    { Type::RED_XIANG, L'相' },
-    { Type::BLACK_XIANG, L'象' },
-    { Type::RED_MA, L'馬' },
-    { Type::BLACK_MA, L'馬' },
-    { Type::RED_JU, L'車' },
-    { Type::BLACK_JU, L'車' },
-    { Type::RED_PAO, L'炮' },
-    { Type::BLACK_PAO, L'炮' },
-    { Type::RED_BING, L'兵' },
-    { Type::BLACK_ZU, L'卒' }
+    { Type::RED_SHUAI, u'帥' },
+    { Type::BLACK_JIANG, u'將' },
+    { Type::RED_SHI, u'仕' },
+    { Type::BLACK_SHI, u'士' },
+    { Type::RED_XIANG, u'相' },
+    { Type::BLACK_XIANG, u'象' },
+    { Type::RED_MA, u'馬' },
+    { Type::BLACK_MA, u'馬' },
+    { Type::RED_JU, u'車' },
+    { Type::BLACK_JU, u'車' },
+    { Type::RED_PAO, u'炮' },
+    { Type::BLACK_PAO, u'炮' },
+    { Type::RED_BING, u'兵' },
+    { Type::BLACK_ZU, u'卒' }
 };
 
 bool Piece::noThreat(const std::map<Pos, std::shared_ptr<Piece>> &context, int x, int y) const
